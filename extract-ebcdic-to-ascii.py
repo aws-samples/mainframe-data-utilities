763fbf497be7883bbce4ace9771f2680616c4413
Luis Gustavo Dantas <luisdant@amazon.com>
2021-11-26 18:36:13 -0300

    preparing for dynamodb option

--- extract-ebcdic-to-ascii.py
+++ extract-ebcdic-to-ascii.py
@@ -13,11 +13,22 @@ def GetLayout(_data, _rules):
     return "transf"
 
 def AddDecPlaces(num,dplaces):
-    return num[:len(num)-dplaces] + '.' + num[len(num)-dplaces:]
+    if dplaces == 0:
+        return num
+    else:
+        return num[:len(num)-dplaces] + '.' + num[len(num)-dplaces:]
 
-print("-----------------------------------------------------","\nParameter file.............|",sys.argv[1])
+arg = dict(zip(sys.argv[1::2], sys.argv[2::2]))
 
-with open(sys.argv[1]) as json_file: param = json.load(json_file)
+if not '-local-json' in arg: 
+    print('\nSintax: python3 extract-ebcdic-to-ascii.py -local-json path/to/layout.json \n')
+    quit()
+
+print("-----------------------------------------------------")
+print("JSON Layout file           |", arg['-local-json'])
+print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f") ,"| STARTED")
+
+with open(arg['-local-json']) as json_file: param = json.load(json_file)
 
 InpF=open(param["input"],"rb")
 OutF=open(param["output"],"w")
@@ -28,7 +39,7 @@ while i < param["max"] or param["max"] == 0:
     linha = InpF.read(param["lrecl"])
 
     if not linha: break
-    
+
     i+= 1
     fim=0
     if i > param["skip"]:
@@ -36,19 +47,13 @@ while i < param["max"] or param["max"] == 0:
         layout = GetLayout(linha, param["transf-rule"])
         
         if(param["print"] != 0 and i % param["print"] == 0): print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f") ,"| Records processed:", i)
-
-        if(i >= param["max"]-3 and i <= param["max"]): print(linha.hex())
-
+        
         ini = 0
         for transf in param[layout]:
 
             fim += transf["bytes"]
 
-            if transf["dplaces"] == 0:
-                OutF.write((ebcdic.unpack(linha[ini:fim],transf["type"], param["rem-low-values"]) + param["separator"]))
-            else:
-                OutF.write((AddDecPlaces(ebcdic.unpack(linha[ini:fim],transf["type"], param["rem-low-values"]), transf["dplaces"]) + param["separator"]))
-            
-            ini = fim
+            OutF.write(AddDecPlaces(ebcdic.unpack(linha[ini:fim],transf["type"], param["rem-low-values"]), transf["dplaces"]) + param["separator"])
 
+            ini = fim
         OutF.write("\n")
