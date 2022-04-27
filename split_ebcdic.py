d52b1160ea1bef36e1ba9bb3a53269e5548bd778
Luis Gustavo Dantas <luisdant@amazon.com>
2022-04-27 18:23:06 -0300

    read from and write to s3

--- split_ebcdic.py
+++ split_ebcdic.py
@@ -7,18 +7,6 @@ import utils, sys, boto3, json
 
 log = utils.Log
 
-def getOutPutFile(rules, data):
-
-    if len(rules) == 0: 
-        log.Write('no rules!')
-        quit()
-
-    for r in rules:
-        if data[r['offset']:r['offset']+r['size']].hex() == r['hex'].lower():
-            return r['file']
-
-    return False
-
 def run(inputfile, lrecl, split_rule, bucket = '', max = 0, skip = 0, print=0):
 
     if bucket != '':
@@ -27,8 +15,12 @@ def run(inputfile, lrecl, split_rule, bucket = '', max = 0, skip = 0, print=0):
         Input=open(inputfile,"rb")
 
     output = {}
+    ctWrit = {}
+    ctRead = 0
+
     for rule in split_rule:
         output[rule['file']] = open(rule['file'],'wb')
+        ctWrit[rule['file']] = 0
 
     i=0
     while max == 0 or i < max:
@@ -40,34 +32,28 @@ def run(inputfile, lrecl, split_rule, bucket = '', max = 0, skip = 0, print=0):
         i+= 1
         if i > skip:
 
-            if (print != 0 and i % print == 0): log.Write(['Records processed', str(i)]) 
-            
-            ofilename = getOutPutFile(split_rule, record)
+            if (print != 0 and i % print == 0): 
+                log.Write(['Records read', str(ctRead)]) 
+                for rule in split_rule:
+                    log.Write(['Records written', rule['file'], str(ctWrit[rule['file']])]) 
 
-            if ofilename:
-                output[ofilename].write(record)
-    
+            if len(split_rule) == 0: 
+                log.Write('no rules!')
+                quit()
+
+            for r in split_rule:
+                if record[r['offset']:r['offset']+r['size']].hex() == r['hex'].lower():
+                    output[r['file']].write(record)
+                    ctWrit[rule['file']] += 1
+
+    for rule in split_rule:
+        if rule['bucket'] != '' and rule['key'] != '':
+            boto3.client('s3').put_object(Body=open(rule['file'], 'rb'), Bucket=rule['bucket'], Key=rule['key'])
+
 if __name__ == '__main__':
 
     arg = dict(zip(sys.argv[1::2], sys.argv[2::2]))
 
     with open(arg['-local-json']) as json_file: param = json.load(json_file)
 
-    run(param['input-file'],param['lrecl'],param['split-rules'])
-
-def lambda_handler(event, context):
-
-    bkt =  event['Records'][0]['s3']['bucket']['name']
-    key =  event['Records'][0]['s3']['object']['key']
-    
-    jfld = ('/' + '/'.join(karr[:-1]) if len(karr := key.split('/')) > 1 else '') + '/layout/'
-    jfle = '.'.join(karr[-1:][0].replace('.txt','').split('.')[:-1]) + '.json'
-
-    run(['extract_ebcdic_to_ascii.py',
-                    '-s3-json' , 's3://' + bkt + jfld + jfle,
-                    '-s3-input', 's3://' + bkt + '/'  + key
-                    ])
-
-    return {'statusCode': 200}
-
-
+    run(param['input-file'],param['lrecl'],param['split-rules'], param['input-bucket'])
