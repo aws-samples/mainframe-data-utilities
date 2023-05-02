eb9338863810fb432d62fae2f1117d10d39315e9
Luis Gustavo Dantas <luisdant@amazon.com>
2023-05-02 05:20:46 -0300

    mdu v2 lambda

---
+++ src/lambda_function.py
@@ -0,0 +1,36 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+import json
+import os
+import core.extract as Extract
+from   core.log import Log
+from   core.cli import CommandLine
+
+json_s3 = os.environ.get('json_s3')
+json_pre = os.environ.get('json_pre')
+work_dir = os.environ.get('work_dir')
+
+def lambda_handler(event, context):
+
+    bkt =  event['Records'][0]['s3']['bucket']['name']
+    key =  event['Records'][0]['s3']['object']['key']
+
+    json = json_pre + key.split('/')[-1].split('.')[0] + '.json'
+
+    cli = CommandLine(
+        [
+        'extract',
+        json,
+        '-json-s3', json_s3,
+        '-input', key,
+        '-input-s3', bkt,
+        '-working-folder', work_dir,
+        '-verbose', 'true'
+        ]
+    )
+
+    log = Log(cli.verbose)
+
+    Extract.FileProcess(log, cli.args)
+
+    return {'statusCode': 200}
